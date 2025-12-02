80b8969f5ec093c968cc33b86dd777ec670899b7
HyungJunChang <hjjang030415@naver.com>
2025-12-03 05:15:08 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css'; // 필요하다면 이 파일에 기본 CSS를 추가합니다.
 import App from './App';
 
